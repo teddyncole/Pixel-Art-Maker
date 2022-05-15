f93d6ccc1d2267808ce441fd1477b5aee6e14958
Teddy Cole <theodorencole@gmail.com>
2022-05-15 18:20:23 -0400

    change makeGrid

--- designs.js
+++ designs.js
@@ -5,16 +5,26 @@
 
 //defining variables from DOM
 const submitButton = document.getElementById('submit');
-const grid = document.getElementById('pixelCanvas');
 var width = document.getElementById('inputWidth');
 var height = document.getElementById('inputHeight');
 const colorPicker = document.getElementById('colorPicker');
 
+
+
 //Adding event listener for color selection
-colorPicker.addEventListener('change', function() {
-    var color = this.value;
-    console.log(color);
-});
+// colorPicker.addEventListener('change', function() {
+//     var color = this.value;
+//     console.log(color);
+// });
+
+colorChange = function(color) {
+    // colorPicker.addEventListener('change', function() {
+    var newColor = color;
+    console.log(newColor);
+    return newColor;
+}
+
+colorPicker.addEventListener('change', colorChange);
 
 //Adding event listener to change grid to chosen height and width
 height.addEventListener('change', function() {
@@ -24,15 +34,26 @@ width.addEventListener('change', function() {
     grid.style.width = width;
 })
 
-
-
-
-
 // When size is submitted by the user, call makeGrid()
 
 
 function makeGrid() {
+    //declaring variables for the grid and cells
+    var grid = document.getElementById('pixelCanvas');
+    // var square = document.createElement('div');
+    // //changing cell color based on chosen one
+    // square.style.color = colorChange();
+    // //making cell border
+    // square.style.border = (solid, black);
 
-// Your code goes here!
-
+    // looping to create grid rows/columns and cells
+    for (var m = grid.style.height; m > 0; m--) {
+        var newRow = grid.insertRow();
+        for (var i = grid.style.width; i > 0; i--) {
+            square = newRow.insertCell();
+        }
+    }
+    for (var n = grid.style.width; n > 0; n--) {
+        
+    }
 }
