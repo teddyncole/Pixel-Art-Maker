5b7cd1d34d1d5990d3e3b9120834ec35630c5ab9
Theodore Cole <104871646+teddyncole@users.noreply.github.com>
2022-05-15 12:36:13 -0400

    add files manually

---
+++ designs.js
@@ -0,0 +1,13 @@
+// Select color input
+// Select size input
+
+const submitButton = document.getElementById('submit');
+const grid = document.getElementById('pixelCanvas');
+const colorPicker = getElementById('colorPicker');
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
