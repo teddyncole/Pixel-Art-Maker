99095eb95748e32ebfb5b5f55b7977addb45d473
Teddy Cole <theodorencole@gmail.com>
2022-05-15 12:54:23 -0400

    commit

--- designs.js
+++ designs.js
@@ -1,9 +1,14 @@
 // Select color input
 // Select size input
 
+//defining variables from DOM
 const submitButton = document.getElementById('submit');
 const grid = document.getElementById('pixelCanvas');
 const colorPicker = getElementById('colorPicker');
+let color = colorPicker.value;
+console.log(color);
+// ADD A VARIABLE FOR COLOR CHOSEN HERE
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
