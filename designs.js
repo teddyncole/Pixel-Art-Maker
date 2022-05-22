4d0aa466de233084520367b8b5f1110c2dd45f5f
Teddy Cole <theodorencole@gmail.com>
2022-05-22 12:51:09 -0400

    editing makeGrid

--- designs.js
+++ designs.js
@@ -20,84 +20,62 @@ colorChange = colorPicker.addEventListener('change', function() {
 
 //Adding event listener to change grid to chosen height and width
 height.addEventListener('change', function() {
-    var rows = document.getElementById('inputHeight').value + 1;
-    const row = document.createElement("tr");
-    const cell = document.createElement('td');
-    row.className = "row"
-    let rowNumber = document.getElementsByClassName("row");
-    for (i = rows; i > 0; i--) {
-        document.getElementById('pixelTable').appendChild(row);
-        row.appendChild(cell);
-    }
+    // var rows = document.getElementById('inputHeight').value + 1;
+    // const row = document.createElement("tr");
+    // const cell = document.createElement('td');
+    // row.className = "row"
+    // let rowNumber = document.getElementsByClassName("row");
+    // for (i = rows; i > 0; i--) {
+    //     document.getElementById('pixelTable').appendChild(row);
+    //     row.appendChild(cell);
+    // }
+    height = document.getElementById('inputHeight').value;
 })
 
 width.addEventListener('change', function() {
-    var columns = document.getElementById('inputWidth').value + 1;
-    const cell = document.createElement('td');
-    let allRows = document.getElementsByClassName("row");
-    let number = 0;
-    for (j = columns; j > 0; j--) {
-        for (i = 0; i < allRows.length; i++) {
-            currentRow = allRows[number];
-            currentRow.appendChild(cell);
-            number++;
-        }
-    }
+    // var columns = document.getElementById('inputWidth').value + 1;
+    // const cell = document.createElement('td');
+    // let allRows = document.getElementsByClassName("row");
+    // let number = 0;
+    // for (j = columns; j > 0; j--) {
+    //     for (i = 0; i < allRows.length; i++) {
+    //         currentRow = allRows[number];
+    //         currentRow.appendChild(cell);
+    //         number++;
+    //     }
+    // }
+    width = document.getElementById('inputWidth').value;
 })
 
 
 
 // TRYING INSTEAD TO ADD A LISTENER TO THE SUBMIT BUTTON AND JUST HAVE THE HEIGHT AND WIDTH LISTENERS STORE THEIR VALUE TO GENERATE THE GRID WHEN THE SUBMIT BUTTON IS PRESSED
 function makeGrid() {
-    let rows = height.value;
-    let columns = width.value;
+    let rows = height - 1;
+    let columns = width;
+    console.log(rows.value);
+    console.log(columns.value);
     let table = document.getElementById('pixelTable');
     for (i = 0; i < rows; i++) {
         var newRow = document.createElement('tr');
         newRow.id = "row[i]";
         table.appendChild(newRow);
     }
-    
+    for (j = 0; j < columns; j++) {
+        var newCell = document.createElement('td');
+        newCell.id = "cell[j]";
+        var tableRows = table.querySelectorAll('tr');
+        var thisRow = tableRows[j];
+        thisRow.appendChild(newCell);
+    }
 }
 
-// makeGrid();
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let columnCount = 1;
-// //add event listener to change grid width
-// width.addEventListener('change', function() {
-//     columnCount += 1;
-//     let toAdd = columnCount;
-//     let rowCount = (document.querySelectorAll('tr').length) + 1;
-//     let allRows = document.getElementsByClassName("row");
-//     const cell = document.createElement('td');
-//     let counter = 0;
-//     let currentRow = document.querySelectorAll('tr')[counter];
-//     let nextRow = currentRow.nextElementSibling;
-//     for (const key in object) {
-//         if (Object.hasOwnProperty.call(object, key)) {
-//             const element = object[key];
-            
-//         }
-//     }
-// })
 
 
 // When size is submitted by the user, call makeGrid()
-// submitButton.addEventListener('click', function() {
-//     makeGrid();
-// })
+submitButton.addEventListener('click', function() {
+    makeGrid();
+})
 
 
 // function makeGrid() {
