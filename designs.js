ee72e492ec294a50dd0bb0f4a713e8e4778843fe
Teddy Cole <theodorencole@gmail.com>
2022-05-22 16:14:31 -0400

    edit calculated row

--- designs.js
+++ designs.js
@@ -17,36 +17,81 @@ colorChange = colorPicker.addEventListener('change', function() {
     return newColor;
 });
 
+function getValue(element) {
+    console.log(element.value);
+    return element.value;
+}
 
 //Adding event listener to change grid to chosen height and width
 height.addEventListener('change', function() {
-    height = document.getElementById('inputHeight').value;
+    height = document.getElementById('inputHeight');
+    console.log(height.value);
 })
 
 width.addEventListener('change', function() {
-    width = document.getElementById('inputWidth').value;
+    width = document.getElementById('inputWidth');
+    console.log(width.value);
 })
+
+//add a class for a row with the right amount of cells and just append the row to the table?
+class CalculatedRow{
+    constructor(cells, rows) {
+        this.cells = cells;
+        this.rows = rows;
+        }
+    
+    createGrid(cells, rows) {
+        let table = document.getElementById('pixelTable');
+        var row = document.createElement('tr');
+        table.appendChild(row);
+        row.id = "row1"
+        for (let i = 0; i < this.cells; i++) {
+            var cell = document.createElement('td');
+            row.appendChild(cell.cloneNode(true));
+        }
+        for (let j = 0; j < (this.rows - 1); j++) {
+            table.appendChild(row.cloneNode(true));
+        }
+    }
+}
+
+
+const firstRow = new CalculatedRow(6,1);
+// firstRow.createRow();
 
 
 
 // TRYING TO ADD A LISTENER TO THE SUBMIT BUTTON AND JUST HAVE THE HEIGHT AND WIDTH LISTENERS STORE THEIR VALUE TO GENERATE THE GRID WHEN THE SUBMIT BUTTON IS PRESSED (to fix: first row doesn't have correct number of cells, if column value is higher than row value, it won't work)
 function makeGrid() {
     let rows = height - 1;
-    let columns = width - 1;
+    let columns = width;
     console.log(rows.value);
     console.log(columns.value);
     let table = document.getElementById('pixelTable');
-    let cell = document.createElement('td');
     for (i = 0; i < rows; i++) {
         var newRow = document.createElement('tr');
         newRow.id = "row[i]";
         table.appendChild(newRow);
-        newRow.appendChild(cell);
         for (j = 0; j < columns; j++) {
             var newCell = document.createElement('td');
-            newCell.id = "cell[j]";
-            newRow.appendChild(newCell);
+            newRow.appendChild(newCell.cloneNode(true));
         }
+    }
+}
+
+
+function createRow(cells) {
+    let table = document.getElementById('pixelTable');
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    table.appendChild(row);
+    counter = cells;
+    if (counter > 0) {
+        row.appendChild(cell);
+        counter--;
+        console.log(counter);
+    } else {
+        return
     }
 }
 
@@ -54,5 +99,6 @@ function makeGrid() {
 
 // When size is submitted by the user, call makeGrid()
 submitButton.addEventListener('click', function() {
-    makeGrid();
+    firstRow.createGrid();
+    
 })
